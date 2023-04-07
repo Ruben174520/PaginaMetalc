a6a410d51f1b596dba84088245abc8d0fbb0af41
Ruben174520 <174520@upslp.edu.mx>
2023-04-06 20:33:59 -0600

    Video en Home

---
+++ metalc/src/app/home/page.js
@@ -0,0 +1,13 @@
+"use client";
+import Video from "../../components/Video";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Prueba de video</h1>
+      <Video />
+    </div>
+  );
+};
+
+export default HomePage;
