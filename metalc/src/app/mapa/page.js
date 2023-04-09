afbfc895658d1e83027256ca0ae580f45dc28745
Noe Liceaga <noe_tetraodron@outlook.com>
2023-04-09 16:38:41 -0600

    Ajuste de navbar en paginas

--- metalc/src/app/mapa/page.js
+++ metalc/src/app/mapa/page.js
@@ -1,11 +1,32 @@
-"use client"
-import Mapa from '@/components/Mapa'
-import React from 'react'
+"use client";
+import Mapa from "@/components/Mapa";
+import React from "react";
+import { useState, useEffect } from "react";
 
 function page() {
+  const [paddingTop, setPaddingTop] = useState(0);
+  const [ancho, setAncho] = useState(0);
+  useEffect(() => {
+    // Obtener la altura de otro-div
+    const alturaNavbar = document.querySelector(".div_navbar").offsetHeight;
+    const anchoNavbar = document.querySelector(".div_navbar").offsetWidth;
+    const a = document.getElementsByClassName("h1_navbar");
+    console.log(a);
+    for (var i = 0; i < a.length; i++) a[i].classList.add('text-stone-950');
+    // Actualizar el estado con el valor de padding-top
+    setPaddingTop(alturaNavbar + 10);
+    setAncho(anchoNavbar);
+  }, []);
   return (
-        <Mapa/>
-  )
+    <div
+      className="h-2/4 flex flex-wrap pt-52 my-0 mx-auto"
+      style={{ paddingTop: paddingTop, width: ancho }}
+    >
+      <div className="w-screen h-2/4 flex flex-wrap">
+        <Mapa />
+      </div>
+    </div>
+  );
 }
 
-export default page
+export default page;
--- metalc/src/components/Navbar.js
+++ metalc/src/components/Navbar.js
@@ -5,36 +5,54 @@ import Image from "next/image";
 import logo from "../icon/logo.png";
 import lupa from "../icon/boxicons/svg/regular/bx-search-alt.svg";
 import mapa from "../icon/boxicons/svg/regular/bx-map.svg";
-import styles from "./Video.module.css"
-
+import styles from "./Video.module.css";
+import { useEffect } from "react";
 export function Navbar() {
-
-    const router = useRouter();
-
+  const router = useRouter();
+  useEffect(() => {
+    const a = document.getElementsByClassName("h1_navbar");
+    console.log(a);
+    for (var i = 0; i < a.length; i++) a[i].classList.remove("text-stone-950");
+  });
   return (
-    <header className="flex w-10/12 z-0 font-bmw justify-between py-4 pt-6 border-b-2 border-cyan-800 text-lg mb-4 absolute left-1/2 -translate-x-1/2">
+    <header
+      className="flex w-10/12 z-0 font-bmw justify-between py-4 pt-6 border-b-2 border-cyan-800 text-lg mb-4 absolute left-1/2 -translate-x-1/2 div_navbar"
+      id="div_navbar"
+    >
       <div className="flex">
         <div className="px-6 cursor-pointer" onClick={() => router.push("/")}>
           <Image src={logo} width={80} height={80} alt="Metalc Logo" />
         </div>
         <div className="flex items-center px-6 mx-10  max-h-14 text-white">
-          <Link href={"/"} className="px-6 hover:border-b-4 border-cyan-800 font-semibold">
-            <h1 className="my-4">Servicios</h1>
+          <Link
+            href={"/"}
+            className="px-6 hover:border-b-4 border-cyan-800 font-semibold"
+          >
+            <h1 className="my-4 h1_navbar">Servicios</h1>
           </Link>
-          <Link href={"/"} className="px-6 hover:border-b-4 border-cyan-800 font-semibold">
-            <h1 className="my-4">Cotizador</h1>
+          <Link
+            href={"/"}
+            className="px-6 hover:border-b-4 border-cyan-800 font-semibold"
+          >
+            <h1 className="my-4 h1_navbar">Cotizador</h1>
           </Link>
-          <Link href={"/"} className="px-6 hover:border-b-4 border-cyan-800 font-semibold">
-            <h1 className="my-4">¿Quienes somos?</h1>
+          <Link
+            href={"/"}
+            className="px-6 hover:border-b-4 border-cyan-800 font-semibold"
+          >
+            <h1 className="my-4 h1_navbar">¿Quienes somos?</h1>
           </Link>
-          <Link href={"/"} className="px-6 hover:border-b-4 border-cyan-800 font-semibold">
-            <h1 className="my-4">Preguntas frecuentes</h1>
+          <Link
+            href={"/"}
+            className="px-6 hover:border-b-4 border-cyan-800 font-semibold"
+          >
+            <h1 className="my-4 h1_navbar">Preguntas frecuentes</h1>
           </Link>
         </div>
       </div>
       <div className="flex items-center mx-10">
         <Link href="/mapa">
-          <Image src={mapa} height={32} width={32} className="mx-2"/>
+          <Image src={mapa} height={32} width={32} className="mx-2" />
         </Link>
         <Link href="/">
           <Image src={lupa} height={32} width={32} />
