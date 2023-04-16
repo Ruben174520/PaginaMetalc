7632bdbc089a7c69674af0a1c544fdb4e6d72dbf
Ruben174520 <174520@upslp.edu.mx>
2023-04-16 15:32:08 -0600

    modificacion Footer

--- metalc/src/components/Footer.js
+++ metalc/src/components/Footer.js
@@ -5,30 +5,44 @@ import face from "../icon/boxicons/svg/logos/bxl-facebook-circle.svg";
 import insta from "../icon/boxicons/svg/logos/bxl-instagram.svg";
 import twitter from "../icon/boxicons/svg/logos/bxl-twitter.svg";
 import whats from "../icon/boxicons/svg/logos/bxl-whatsapp.svg";
+import ubi from "../icon/boxicons/svg/regular/bx-mapGreen.svg";
+import reloj from "../icon/boxicons/svg/regular/bx-time-fiveGreen.svg";
+import tel from "../icon/boxicons/svg/regular/bx-phone-callGreen.svg";
+import email from "../icon/boxicons/svg/regular/bx-envelopeGreen.svg";
 
 
 function Footer(){
     return(
-        <footer className={"font-bmw" + styles.footer}>
+        <footer className={styles.footer}>
         <div className={styles.containerFather}>
+          <div className={styles.bord}>
             <div className={styles.hijo}>
-              <li className={styles.titulo + "font-weight:bold" }>MENU</li>
-                <li><Link className={styles.a} href="#">Inicio</Link></li>
-                <li><Link className={styles.a} href="#">Servicios</Link></li>
-                <li><Link className={styles.a} href="#">Cotizador</Link></li>
+              <div className={styles.adaptar}>
+                <li className={styles.titulo}><Image src={ubi} height={40} width={40} className="mx-2"/></li>
+                <li><Link className={styles.a} href="#">Av. Seminario 256 Fracc, Las Mercedes, 78394 San Luis, S.L.P.</Link></li>
+              </div>            
             </div>
             <div className={styles.hijo}>
-              <li className={styles.titulo + "font-weight:bold" }>ACERCA DE</li>
-                <li><Link className={styles.a} href="#">¿Quienes somos?</Link></li>
-                <li><Link className={styles.a} href="#">Preguntas Frecuentes</Link></li>
-                <li><Link className={styles.a} href="#">Ubicacion</Link></li>
+              <div className={styles.adaptar}>
+                <li className={styles.titulo}><Image src={reloj} height={35} width={35} className="mx-2"/></li>
+                <li>Lun-Vie:8:00 hrs a 18:00 hrs Sabado: 8:00 hrs a 14:00 hrs</li>
+              </div>
             </div>
             <div className={styles.hijo}>
-              <li className={styles.titulo + "font-weight:bold" }>CONTACTO</li>
-              <li className={"color: gray"}>Email: vero.metalce@gmail.com</li>
-              <li className={"color: gray"}><Link href="https://metalc.com.mx">Metalc San Luis Potosí</Link></li>
+              <div className={styles.adaptar}>
+                <li className={styles.titulo}><Image src={tel} height={30} width={30} className="mx-2"/></li>
+                <li className={"color: gray"}>Whatsapp: 444 655 5651 Oficina: 444 111 1111</li>
+              </div>
             </div>
-          </div>
+            <div className={styles.hijo}>
+              <div className={styles.adaptar}>
+                <li className={styles.titulo}><Image src={email} height={30} width={30} className="mx-2"/></li>
+                <li>vero.metalc@gmail.com</li>
+              </div>
+            </div>
+            </div>
+          </div >
+          
           <div className={styles.redes}>
             <div className={styles.redHijo}>
               <li className={styles.redHijoli}><Link href=""><Image src={face} typeof="logo" height={25} width={25} className={styles.logo} /></Link></li>
@@ -37,6 +51,7 @@ function Footer(){
               <li className={styles.redHijoli}><Link href=""><Image src={whats} height={25} width={25} className={"color='#ffffff'"} /></Link></li>            
             </div>
           </div>
+          
         </footer>
     )
 }
