fa8444f7010bc487f202b242e546cdd43d166e59
Ruben174520 <174520@upslp.edu.mx>
2023-04-22 10:38:44 -0600

    Carrusel Servicio

--- metalc/src/app/servicios/page.js
+++ metalc/src/app/servicios/page.js
@@ -13,7 +13,20 @@ function page() {
   const images = [
     { id: 1, url: 'https://aluminioindustrial.mx/blog/wp-content/uploads/2019/09/perfiles-aluminio-varios.jpg' },
     { id: 2, url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQH4rF_6ZqA0PtDxX9G8iENwAlR9zGaQUO6VA&usqp=CAU' },
-    { id: 3, url: 'https://i.blogs.es/14d603/cobre1/1366_2000.jpeg' }
+    { id: 3, url: 'https://i.blogs.es/14d603/cobre1/1366_2000.jpeg' },
+    { id: 4, url: 'https://media.istockphoto.com/id/868926884/es/foto/viejas-partes-de-coches-oxidados-oxidados-en-desguace-de-coches-coche-reciclado-piezas-de.jpg?s=612x612&w=0&k=20&c=hyTIObkdAxG-bMxWATlMNcY6F5_cIGdwtpFB-wU5pTU=' },
+    { id: 5, url: 'https://www.elchatarrero.com/wp-content/uploads/2017/06/tulio.jpg' },
+    { id: 6, url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc3awL11Mn9j-DPSo500_AEBKS1IlGyojC2Q&usqp=CAU' }
+  ];
+
+  const names = [
+    {id:'1', metal: "Aluminio", estilo: "font-bmw", posicion: "display: 'flex' align-text: 'end'"},
+    {id:'2', metal: "Bronce", estilo: "font-bmw", posicion: "display: 'flex' align-text: 'end'"},
+    {id:'3', metal: "Cobre", estilo: "font-bmw", posicion: "display: 'flex' align-text: 'end'"},
+    {id:'4', metal: "Chatarra", estilo: "font-bmw", posicion: "display: 'flex' align-text: 'end'"},
+    {id:'5', metal: "Rebabas", estilo: "font-bmw", posicion: "display: 'flex' align-text: 'end'"},
+    {id:'6', metal: "Metales Raros", estilo: "font-bmw", posicion: "display: 'flex' align-text: 'end'"},
+    {id:'7', metal: "Aceros inoxidables", estilo: "font-bmw", posicion: "display: 'flex' align-text: 'end'"}
   ];
 
     return (
@@ -30,7 +43,7 @@ function page() {
                 <div className={servicios.scrap}>
 
                     <div className={servicios.textoScrap}>
-                        <div>
+                        <div className="border-2 border-blue-500 w-[50%] pr-[20px]">
                             <h1 className="font-bmw text-2xl ">RECOLECCIÓN DE SCRAPT</h1><br />
                             <p className="justify-text-left">Uno de los principales servicios que Metalc puede brindarte, es la compra de scrap.
                                 Puedes acudir directamente a la empresa a realizar una cotizacion del material que tienes disponible. <br />
@@ -40,7 +53,7 @@ function page() {
                         <div>
                             
                         </div>
-                        <div className="w-[50%] h-[400px]">
+                        <div className="h-[400px] w-[50%] ">
                             <ImageCarousel images={images} />
                         </div>
                     </div>
--- metalc/src/components/ImageCarousel.js
+++ metalc/src/components/ImageCarousel.js
@@ -1,29 +1,61 @@
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import confArrow from "./ConfArrow.module.css";
+
+function SampleNextArrow(props) {
+  
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "black",
+        color: "white",
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "black" }}
+      onClick={onClick}
+    />
+  );
+}
 
 function ImageCarousel({ images }) {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
-  
-    return (
-      <Slider {...settings}>
-        {images.map((image) => (
-          <div key={image.id}>
-            <img src={image.url} />
-          </div>
-        ))}
-      </Slider>
-    );
-  }
-  
-  export default ImageCarousel;
-  
-  
-  
-  
+  const slider = React.useRef(null);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+
+  return (
+    <Slider ref={slider} {...settings}>
+      {images.map((image) => (
+        <div key={image.id}>
+          <img src={image.url} className="h-[400px] w-[100%] object-fit " />
+          <h1></h1>
+        </div>
+      ))}
+      
+    </Slider>
+  );
+}
+
+export default ImageCarousel;
