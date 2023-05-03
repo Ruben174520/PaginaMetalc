0b7ee487e97c59c60127df595126f76ba5111e7f
Ruben174520 <174520@upslp.edu.mx>
2023-05-03 00:20:25 -0600

    cambios semana 4

--- metalc/src/components/Footer.js
+++ metalc/src/components/Footer.js
@@ -51,8 +51,6 @@ function Footer(){
             </div>
             <div className={styles.redHijo}>
               <li className={styles.redHijoli}><Link href=""><Image src={face} typeof="logo" height={25} width={25} className={styles.logo} /></Link></li>
-              <li className={styles.redHijoli}><Link href=""><Image src={insta} height={25} width={25} className={"color='#ffffff'"} /></Link></li>
-              <li className={styles.redHijoli}><Link href=""><Image src={twitter} height={25} width={25} className={"color='#ffffff'"} /></Link></li>
               <li className={styles.redHijoli}><Link href=""><Image src={whats} height={25} width={25} className={"color='#ffffff'"} /></Link></li>            
             </div>
           </div>
--- metalc/src/components/RespSocial.js
+++ metalc/src/components/RespSocial.js
@@ -2,14 +2,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import { Card } from 'react-bootstrap';
+import Image from 'next/image';
 import Foto from '../img/cobre.jpg';
+import reciclar from '../img/reciclar.jpg'
+import reutilizar from '../img/reutilizar.jpg'
+import reducir from '../img/reducir.jpg'
+import recuperar from '../img/recuperar.jpg'
 function RespSocial() {
   return (
+    
     <div className="h-[90%] block px-[50px] mt-5 mb-10">
         <div className='font-bmw  text-[40px] flex justify-center h-[20%]'>RESPONSABILIDAD SOCIAL</div>
+        
         <div className='flex justify-between h-[80%] '>
             <Card style={{ width: '18rem', height: '100%', transition: 'transform .3s ease-out' }} className='hover:-translate-y-4'>
-            <Card.Img variant="top" src= {"https://img.freepik.com/vector-premium/simbolo-reciclaje_157999-198.jpg?w=2000"} className='h-[180px] object-cover' />
+            <Image variant="top" src={reciclar}  className='h-[180px] object-cover'/> 
             <Card.Body>
                 <Card.Title className='mb-4'>Reciclar</Card.Title>
                 <Card.Text className='font-bmw mb-4'>
@@ -22,7 +29,7 @@ function RespSocial() {
             </Card>
 
             <Card style={{ width: '18rem', height: '100%', transition: 'transform .3s ease-out' }} className='hover:-translate-y-4'>
-            <Card.Img variant="top" src= {"https://4.bp.blogspot.com/-_oKVRyJ-ojc/V10MM8_1bwI/AAAAAAAABQY/rjz-QDZoOdYtWmgD77OFSvODkF6u-j2iwCK4B/s1600/slide_492310_6795188_free.jpg"} className='h-[180px] object-cover' />
+            <Image variant="top" src={reutilizar}  className='h-[180px] object-cover'/> 
             <Card.Body>
                 <Card.Title className='mb-4'>Reutilizar</Card.Title>
                 <Card.Text className='font-bmw mb-4'>
@@ -35,7 +42,7 @@ function RespSocial() {
             </Card>
 
             <Card style={{ width: '18rem', height: '100%', transition: 'transform .3s ease-out' }} className='hover:-translate-y-4'>
-            <Card.Img variant="top" src= {"https://static.vecteezy.com/system/resources/previews/016/138/289/non_2x/reducing-co2-emissions-icon-stop-climate-change-sign-for-graphic-design-logo-website-social-media-mobile-app-ui-illustration-vector.jpg"} className='h-[180px] object-cover' />
+            <Image variant="top" src={reducir}  className='h-[180px] object-cover'/> 
             <Card.Body>
                 <Card.Title className='mb-4' >Reducir</Card.Title>
                 <Card.Text className='font-bmw mb-4'>
@@ -48,11 +55,11 @@ function RespSocial() {
             </Card>
 
             <Card style={{ width: '18rem', height: '100%', transition: 'transform .3s ease-out' }} className='hover:-translate-y-4'>
-            <Card.Img variant="top" src= {"https://ecologiaymedioambiente11a.files.wordpress.com/2015/08/ecologia.jpg"} className='h-[180px] object-cover' />
+            <Image variant="top" src={recuperar}  className='h-[180px] object-cover'/> 
             <Card.Body>
                 <Card.Title className='mb-4' >Recuperar</Card.Title>
                 <Card.Text className='font-bmw mb-4'>
-                Dentro de las instalaciones de Metalc, ayudamos a la reducción de los servicios básicos con los que cuenta la empresa.(Luz, Agua, Gas).
+                En nuestra empresa, estamos comprometidos con reducir nuestra huella ambiental y promover prácticas sostenibles en cada aspecto de nuestro negocio. 
                 </Card.Text>
                 <div className="d-grid gap-2">
                     <Button variant="primary" size='sm' className='text-black font-bmw bg-[#1a7ab6] hover:bg-[#8bba1f]'>Contáctanos</Button>
