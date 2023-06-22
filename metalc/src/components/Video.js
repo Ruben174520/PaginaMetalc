89c851c4741782f289efbf520693184d68d2ea91
Ruben174520 <174520@upslp.edu.mx>
2023-06-21 23:23:40 -0600

    video

--- metalc/src/components/Video.js
+++ metalc/src/components/Video.js
@@ -4,7 +4,7 @@ const Video = () => {
   return (
     <video className={styles.video} controls={false} autoPlay loop muted>
       <source
-        src="https://github.com/Ruben174520/nextjs-blog-metalc/blob/main/metalc/src/img/Inicio.mp4"
+        src="https://aventourslp.com.mx/Inicio.mp4"
         type="video/mp4"
       />
       Your browser does not support the video tag.
