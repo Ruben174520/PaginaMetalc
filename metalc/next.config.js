f34a524a7818d533ed3aa98be104e6d91b71432b
Ruben174520 <174520@upslp.edu.mx>
2023-06-21 22:56:33 -0600

    cambio

--- metalc/next.config.js
+++ metalc/next.config.js
@@ -3,21 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.(mp4|webm)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            publicPath: '/_next',
-            name: 'static/media/[name].[hash].[ext]',
-          },
-        },
-      });
-    }
-    return config;
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
